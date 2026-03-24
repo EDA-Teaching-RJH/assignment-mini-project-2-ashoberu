d4a578e97f0b5b8d959ac5b606c666ce99c87de7
ashoberu <as113@kent.ac.uk>
2026-03-24 16:53:23 +0000

    to test the email regex

--- test_app.py
+++ test_app.py
@@ -3,4 +3,9 @@ from custom_utils import validate_email, validate_student_id
 from models import Student, Course
 
 def test_validate_student_id():
+    assert validate_student_id("comp1234") == True
+    assert validate_student_id("comp123") == False
+    assert validate_student_id("1234comp") == False
+
+def test_validate_email():
     
