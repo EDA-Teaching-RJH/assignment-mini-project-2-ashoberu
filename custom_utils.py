f27aee3235fcbe68294f513079e32cf4895f9d00
ashoberu <as113@kent.ac.uk>
2026-03-24 15:05:43 +0000

    reads data from a CSV and returns a list of dictionaries

--- custom_utils.py
+++ custom_utils.py
@@ -11,4 +11,16 @@ def validates_email(email):
     return bool(re.search(pattern, email))
 
 def save_to_csv(filename, data_list):
+    if not data_list:
+        return
+    keys = data_list[0].keys()
+    file_exists = os.path.isfile(filename)
+
+    with open(filename, 'a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=keys)
+        if not file_exists:
+            writer.writeheader()
+        writer.writerows(data_list)
+
+def load_from_csv(filename):
     
