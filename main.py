48682080a3f153cab9997f0417f4e685cd51ba41
ashoberu <as113@kent.ac.uk>
2026-03-24 17:18:37 +0000

    this uses matplotlib to visualise loaded CSV data

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ from models import Student, GraduateStudent
 from custom_utils import save_to_csv, load_from_csv
 
 def fetch_advisor():
+    try:
+        response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+        if response.status_code == 200:
+            return json.loads(response.text)["name"]
+    except Exception:
+        pass
+    return "TBD"
+
+def plot_degree_distribution(students_data):
     
