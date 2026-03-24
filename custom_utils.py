1d47d650ae5c19a483ec8233f4e573bb1230764c
ashoberu <as113@kent.ac.uk>
2026-03-24 14:54:51 +0000

    validates a university email address

--- custom_utils.py
+++ custom_utils.py
@@ -3,4 +3,8 @@ import csv
 import os
 
 def validate_student_id(student_id):
+    pattern = r"^[a-zA-Z]{4}\d{4}$"
+    return bool(re.search(pattern, student_id))
+
+def validates_email(email):
     
